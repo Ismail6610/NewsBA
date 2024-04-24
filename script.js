97e6a83ba73b644735bfc5fe61207fedf1b082fc
Ismail Bajrovic <115583520+Ismail6610@users.noreply.github.com>
2024-04-24 10:15:43 +0200

    wada
    dwada

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const newsList = document.querySelector('.container');
 
     function displayNews(news) {
+        newsList.innerHTML=' ';
+
+
         news.forEach(newsItem => {
             const newItem = document.createElement('div');
             newItem.classList.add('news');
@@ -20,6 +23,23 @@ document.addEventListener('DOMContentLoaded', function() {
             newsList.appendChild(newItem);
         });
     }
+    /*function filterNews(search) {
+        const filteredNews = newsData.filter(newsItem => newsItem.title.toLowerCase().includes(search.toLowerCase()));
+        displayNews(filteredNews);
+    }
+
+
+    
+    document.getElementById('search').addEventListener('input', function() {
+        const search = this.value.trim();
+        if (search === '') {
+            displayNews(newsData); 
+        } else {
+            filterNews(search);
+        }
+    });*/
+
+
     const newsData = [
         {
             imageUrl: "./assets/klix.jpg",
@@ -43,17 +63,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const newsList = document.querySelector('.bigContainer');
 
     function displayNews(news) {
-        news.forEach(newsItem => {
+        newsList.innerHTML=' ';
+
+        news.forEach(newsItem2 => {
             const newItem = document.createElement('div');
             newItem.classList.add('news');
 
             newItem.innerHTML = `
-            <div class="news-container">
-                <img src="${newsItem.imageUrl}" alt="Image">
-                <div class="news-content">
-                    <h2>${newsItem.title}</h2>
-                    <p>${newsItem.description}</p>
-                    <a href="${newsItem.link}" class="read-more">Read more</a>
+            <div class="bigNews-container">
+                <img src="${newsItem2.imageUrl}" alt="Image">
+                <div class="bigNews-content">
+                    <h2>${newsItem2.title}</h2>
+                    <p>${newsItem2.description}</p>
+                    <a href="${newsItem2.link}" class="bigRead-more">Read more</a>
                 </div>
             </div>
             `;
@@ -62,12 +84,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    
+
 
     const newsDataBig = [
         {
             imageUrl: "./assets/klix7.jpg",
-            title: "ZELJEZNICAR POBJEDIO SARAJEVO",
-            description: "U jutarnjim satima desila se nesreća...",
+            title: "HAOS NA GRBAVICI",
+            description: "Zeljeznicar pobjedio rivala sa nevjerovatnih 12-0",
             link: "./vijesti/sudar.html"
         }
     ];
