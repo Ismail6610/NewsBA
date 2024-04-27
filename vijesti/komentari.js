c6bba1e40193c83807a3ae033a5d338566755265
skisord <167910824+skisord@users.noreply.github.com>
2024-04-27 10:50:21 +0200

    dd
    e

---
+++ vijesti/komentari.js
@@ -0,0 +1,8 @@
+function spasikommnetar(){
+    var kommentar= document.getElementById("kommentar1").value;
+
+    sessionStorage.setItem("kommentar", kommentar);
+
+    console.log(sessionStorage.getItem("kommentar"))
+
+}
